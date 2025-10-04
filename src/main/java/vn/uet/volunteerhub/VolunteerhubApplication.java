136fd770ccdf5c182c22ad5f4932cce58824c5da
LeMinhDuc47 <fmgduc47@gmail.com>
2025-10-04 13:04:48 +0700

    initialize

---
+++ src/main/java/vn/uet/volunteerhub/VolunteerhubApplication.java
@@ -0,0 +1,17 @@
+package vn.uet.volunteerhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+})
+
+public class VolunteerhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolunteerhubApplication.class, args);
+	}
+
+}
