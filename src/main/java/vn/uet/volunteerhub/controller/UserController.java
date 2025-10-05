d2b9b94f7064422ef65256a8be5c5d1afaa3b726
LeMinhDuc47 <fmgduc47@gmail.com>
2025-10-06 00:43:49 +0700

    add Json web token

--- src/main/java/vn/uet/volunteerhub/controller/UserController.java
+++ src/main/java/vn/uet/volunteerhub/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,13 +21,17 @@ import vn.uet.volunteerhub.service.error.IdInvalidException;
 public class UserController {
     // Dependency Injection
     private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, PasswordEncoder passwordEncoder) {
         this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @PostMapping("/users")
     public ResponseEntity<User> createNewUser(@RequestBody User createUser) {
+        String hashPasword = this.passwordEncoder.encode(createUser.getPassword());
+        createUser.setPassword(hashPasword);
         User newUser = this.userService.handleCreateUser(createUser);
         return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
     }
