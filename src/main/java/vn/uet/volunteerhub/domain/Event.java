0c5f2675fbe8c57a637722414a7a74b57c8b05e0
LeMinhDuc47 <fmgduc47@gmail.com>
2025-10-08 01:16:06 +0700

    Handle before persist data

--- src/main/java/vn/uet/volunteerhub/domain/Event.java
+++ src/main/java/vn/uet/volunteerhub/domain/Event.java
@@ -2,15 +2,19 @@ package vn.uet.volunteerhub.domain;
 
 import java.time.Instant;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
+import vn.uet.volunteerhub.util.SecurityUtil;
 
 @Entity
 @Table(name = "events")
@@ -32,9 +36,19 @@ public class Event {
 
     private Instant createdAt;
 
-    private Instant updateddAt;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss a", timezone = "GMT+7")
+    private Instant updatedAt;
 
     private String createdBy;
 
     private String updatedBy;
+
+    @PrePersist
+    public void handleBeforeCreated() {
+        String userLogin = SecurityUtil.getCurrentUserLogin().isPresent() == true
+                ? SecurityUtil.getCurrentUserLogin().get()
+                : "";
+        this.createdBy = userLogin;
+        this.createdAt = Instant.now();
+    }
 }
