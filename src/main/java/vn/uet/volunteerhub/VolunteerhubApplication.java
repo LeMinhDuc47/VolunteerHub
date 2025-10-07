2a3ef23ffd0b6085bbbc10b0509cd8481904762b
LeMinhDuc47 <fmgduc47@gmail.com>
2025-10-07 08:30:21 +0700

    Decode Token and Config Oauth2ResourceServe to Protect Endpoint

--- src/main/java/vn/uet/volunteerhub/VolunteerhubApplication.java
+++ src/main/java/vn/uet/volunteerhub/VolunteerhubApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 // 		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
 // 		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
 // })
-
+@SpringBootApplication
 public class VolunteerhubApplication {
 
 	public static void main(String[] args) {
