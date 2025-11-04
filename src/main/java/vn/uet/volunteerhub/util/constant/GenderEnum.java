991cfef2cabc6658085d9c36b000bfeb6b5eeb10
LeMinhDuc47 <fmgduc47@gmail.com>
2025-11-04 07:03:51 +0700

    update user use enum

---
+++ src/main/java/vn/uet/volunteerhub/util/constant/GenderEnum.java
@@ -0,0 +1,8 @@
+package vn.uet.volunteerhub.util.constant;
+
+public enum GenderEnum {
+    // Khai báo constant
+    FEMALE, MALE, OTHER;
+
+    // Enum: https://dev.to/noelopez/spring-rest-working-with-enums-ma
+}
