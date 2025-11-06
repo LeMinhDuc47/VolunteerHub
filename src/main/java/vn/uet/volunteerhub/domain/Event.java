1c4ff773c73a789ad7cf2088c71cee8d3fb30dcb
LeMinhDuc47 <fmgduc47@gmail.com>
2025-11-06 16:31:53 +0700

    Model Relationships: Associations User entity and Company entity

--- src/main/java/vn/uet/volunteerhub/domain/Event.java
+++ src/main/java/vn/uet/volunteerhub/domain/Event.java
@@ -6,9 +6,11 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
@@ -43,6 +45,8 @@ public class Event {
     private String createdBy;
 
     private String updatedBy;
+    @OneToMany(mappedBy = "company", fetch = FetchType.LAZY)
+    private List<User> users;
 
     @PrePersist
     public void handleBeforeCreate() {
