aa1aae4edda538c9237e0e80d2afe3c77cc1f90d
LeMinhDuc47 <fmgduc47@gmail.com>
2025-11-07 13:42:02 +0700

    Modules Job/Resume: API Create New Resume

---
+++ src/main/java/vn/uet/volunteerhub/domain/response/resume/ResCreateResumeDTO.java
@@ -0,0 +1,14 @@
+package vn.uet.volunteerhub.domain.response.resume;
+
+import java.time.Instant;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ResCreateResumeDTO {
+    private Long id;
+    private Instant createdAt;
+    private String createdBy;
+}
--- src/main/java/vn/uet/volunteerhub/service/ResumeService.java
+++ src/main/java/vn/uet/volunteerhub/service/ResumeService.java
@@ -1,15 +1,61 @@
 package vn.uet.volunteerhub.service;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
+import vn.uet.volunteerhub.domain.Job;
+import vn.uet.volunteerhub.domain.Resume;
+import vn.uet.volunteerhub.domain.User;
+import vn.uet.volunteerhub.domain.response.resume.ResCreateResumeDTO;
+import vn.uet.volunteerhub.repository.JobRepository;
 import vn.uet.volunteerhub.repository.ResumeRepository;
+import vn.uet.volunteerhub.repository.UserRepository;
 
 @Service
 public class ResumeService {
 
     private final ResumeRepository resumeRepository;
+    private final UserRepository userRepository;
+    private final JobRepository jobRepository;
 
-    public ResumeService(ResumeRepository resumeRepository) {
+    public ResumeService(ResumeRepository resumeRepository, UserRepository userRepository,
+            JobRepository jobRepository) {
         this.resumeRepository = resumeRepository;
+        this.userRepository = userRepository;
+        this.jobRepository = jobRepository;
+    }
+
+    public boolean checkResumeExistByUserAndJob(Resume requestResume) {
+        // check user by id
+        if (requestResume.getUser() == null) {
+            return false;
+        }
+        Optional<User> userOptional = this.userRepository.findById(requestResume.getUser().getId());
+        if (!userOptional.isPresent()) {
+            return false;
+        }
+
+        // check job by id
+        if (requestResume.getJob() == null) {
+            return false;
+        }
+        Optional<Job> jobOptional = this.jobRepository.findById(requestResume.getJob().getId());
+        if (!jobOptional.isPresent()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public ResCreateResumeDTO handleCreateResume(Resume requestResume) {
+        Resume newResume = this.resumeRepository.save(requestResume);
+
+        ResCreateResumeDTO dto = new ResCreateResumeDTO();
+        dto.setId(newResume.getId());
+        dto.setCreatedAt(newResume.getCreatedAt());
+        dto.setCreatedBy(newResume.getCreatedBy());
+
+        return dto;
     }
 }
