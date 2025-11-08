c1b46a94148a0adb50cb813c7a056d5552350090
LeMinhDuc47 <fmgduc47@gmail.com>
2025-11-08 10:19:08 +0700

    Modules Subcribers: Create Entity Subcribers and Relationships Entity Skills

--- src/main/java/vn/uet/volunteerhub/domain/Subscriber.java
+++ src/main/java/vn/uet/volunteerhub/domain/Subscriber.java
@@ -2,15 +2,16 @@ package vn.uet.volunteerhub.domain;
 
 import java.time.Instant;
 import java.util.List;
-import java.util.concurrent.Flow.Subscriber;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
@@ -21,27 +22,29 @@ import lombok.Setter;
 import vn.uet.volunteerhub.util.SecurityUtil;
 
 @Entity
-@Table(name = "skills")
+@Table(name = "subscribers")
 @Getter
 @Setter
-public class Skill {
+public class Subscriber {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @NotBlank(message = "email không được để trống")
+    private String email;
+
     @NotBlank(message = "name không được để trống")
     private String name;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JsonIgnoreProperties(value = { "Subscribers" })
+    @JoinTable(name = "subscriber_skill", joinColumns = @JoinColumn(name = "subscriber_id"), inverseJoinColumns = @JoinColumn(name = "skill_id"))
+    private List<Skill> skills;
 
     private Instant createdAt;
     private Instant updatedAt;
     private String createdBy;
     private String updatedBy;
-
-    @ManyToMany(fetch = FetchType.LAZY, mappedBy = "skills")
-    @JsonIgnore
-    private List<Job> jobs;
-    @ManyToMany(fetch = FetchType.LAZY, mappedBy = "skills")
-    @JsonIgnore
-    private List<Subscriber> subscribers;
 
     @PrePersist
     public void handleBeforeCreate() {
@@ -60,5 +63,4 @@ public class Skill {
         this.updatedBy = userLogin;
         this.updatedAt = Instant.now();
     }
-
 }
