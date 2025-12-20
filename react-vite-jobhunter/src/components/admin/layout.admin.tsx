71d6a3eb35a4437f50b159955d1af75a7bef07f1
csDune05 <dungduoc123bn@gmail.com>
2025-12-20 23:28:16 +0700

    commit

--- react-vite-jobhunter/src/components/admin/layout.admin.tsx
+++ react-vite-jobhunter/src/components/admin/layout.admin.tsx
@@ -1,15 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import {
-    AppstoreOutlined,
-    ExceptionOutlined,
-    ApiOutlined,
-    UserOutlined,
-    BankOutlined,
-    MenuFoldOutlined,
-    MenuUnfoldOutlined,
-    AliwangwangOutlined,
-    BugOutlined,
-    ScheduleOutlined,
+  AppstoreOutlined,
+  ExceptionOutlined,
+  ApiOutlined,
+  UserOutlined,
+  BankOutlined,
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  AliwangwangOutlined,
+  ScheduleOutlined,
 } from '@ant-design/icons';
 import { Layout, Menu, Dropdown, Space, message, Avatar, Button } from 'antd';
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
@@ -21,204 +20,197 @@ import type { MenuProps } from 'antd';
 import { setLogoutAction } from '@/redux/slice/accountSlide';
 import { ALL_PERMISSIONS } from '@/config/permissions';
 
+import uetLogo from '@/assets/uet-logo.png';
+import "@/styles/components/admin/layout_style.css"
+
 const { Content, Sider } = Layout;
 
 const LayoutAdmin = () => {
-    const location = useLocation();
+  const location = useLocation();
 
-    const [collapsed, setCollapsed] = useState(false);
-    const [activeMenu, setActiveMenu] = useState('');
-    const user = useAppSelector(state => state.account.user);
+  const [collapsed, setCollapsed] = useState(false);
+  const [activeMenu, setActiveMenu] = useState('');
+  const user = useAppSelector(state => state.account.user);
 
-    const permissions = useAppSelector(state => state.account.user.role.permissions);
-    const [menuItems, setMenuItems] = useState<MenuProps['items']>([]);
+  const permissions = useAppSelector(state => state.account.user.role.permissions);
+  const [menuItems, setMenuItems] = useState<MenuProps['items']>([]);
 
-    const navigate = useNavigate();
-    const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
 
-    useEffect(() => {
-        const ACL_ENABLE = import.meta.env.VITE_ACL_ENABLE;
-        if (permissions?.length || ACL_ENABLE === 'false') {
+  useEffect(() => {
+    const ACL_ENABLE = import.meta.env.VITE_ACL_ENABLE;
+    if (permissions?.length || ACL_ENABLE === 'false') {
+      const viewEvent = permissions?.find(item =>
+        item.apiPath === ALL_PERMISSIONS.EVENTS.GET_PAGINATE.apiPath
+        && item.method === ALL_PERMISSIONS.EVENTS.GET_PAGINATE.method
+      );
 
-            const viewEvent = permissions?.find(item =>
-                item.apiPath === ALL_PERMISSIONS.EVENTS.GET_PAGINATE.apiPath
-                && item.method === ALL_PERMISSIONS.EVENTS.GET_PAGINATE.method
-            )
+      const viewUser = permissions?.find(item =>
+        item.apiPath === ALL_PERMISSIONS.USERS.GET_PAGINATE.apiPath
+        && item.method === ALL_PERMISSIONS.USERS.GET_PAGINATE.method
+      );
 
-            const viewUser = permissions?.find(item =>
-                item.apiPath === ALL_PERMISSIONS.USERS.GET_PAGINATE.apiPath
-                && item.method === ALL_PERMISSIONS.USERS.GET_PAGINATE.method
-            )
+      const viewJob = permissions?.find(item =>
+        item.apiPath === ALL_PERMISSIONS.JOBS.GET_PAGINATE.apiPath
+        && item.method === ALL_PERMISSIONS.JOBS.GET_PAGINATE.method
+      );
 
-            const viewJob = permissions?.find(item =>
-                item.apiPath === ALL_PERMISSIONS.JOBS.GET_PAGINATE.apiPath
-                && item.method === ALL_PERMISSIONS.JOBS.GET_PAGINATE.method
-            )
+      const viewResume = permissions?.find(item =>
+        item.apiPath === ALL_PERMISSIONS.RESUMES.GET_PAGINATE.apiPath
+        && item.method === ALL_PERMISSIONS.RESUMES.GET_PAGINATE.method
+      );
 
-            const viewResume = permissions?.find(item =>
-                item.apiPath === ALL_PERMISSIONS.RESUMES.GET_PAGINATE.apiPath
-                && item.method === ALL_PERMISSIONS.RESUMES.GET_PAGINATE.method
-            )
+      const viewRole = permissions?.find(item =>
+        item.apiPath === ALL_PERMISSIONS.ROLES.GET_PAGINATE.apiPath
+        && item.method === ALL_PERMISSIONS.ROLES.GET_PAGINATE.method
+      );
 
-            const viewRole = permissions?.find(item =>
-                item.apiPath === ALL_PERMISSIONS.ROLES.GET_PAGINATE.apiPath
-                && item.method === ALL_PERMISSIONS.ROLES.GET_PAGINATE.method
-            )
+      const viewPermission = permissions?.find(item =>
+        item.apiPath === ALL_PERMISSIONS.PERMISSIONS.GET_PAGINATE.apiPath
+        && item.method === ALL_PERMISSIONS.USERS.GET_PAGINATE.method
+      );
 
-            const viewPermission = permissions?.find(item =>
-                item.apiPath === ALL_PERMISSIONS.PERMISSIONS.GET_PAGINATE.apiPath
-                && item.method === ALL_PERMISSIONS.USERS.GET_PAGINATE.method
-            )
+      const full: MenuProps['items'] = [
+        {
+          label: <Link to='/admin'>Dashboard</Link>,
+          key: '/admin',
+          icon: <AppstoreOutlined />
+        },
+        ...(viewEvent || ACL_ENABLE === 'false' ? [{
+          label: <Link to='/admin/event'>Event</Link>,
+          key: '/admin/event',
+          icon: <BankOutlined />,
+        }] : []),
 
-            const full = [
-                {
-                    label: <Link to='/admin'>Dashboard</Link>,
-                    key: '/admin',
-                    icon: <AppstoreOutlined />
-                },
-                ...(viewEvent || ACL_ENABLE === 'false' ? [{
-                    label: <Link to='/admin/event'>Event</Link>,
-                    key: '/admin/event',
-                    icon: <BankOutlined />,
-                }] : []),
+        ...(viewUser || ACL_ENABLE === 'false' ? [{
+          label: <Link to='/admin/user'>User</Link>,
+          key: '/admin/user',
+          icon: <UserOutlined />
+        }] : []),
 
-                ...(viewUser || ACL_ENABLE === 'false' ? [{
-                    label: <Link to='/admin/user'>User</Link>,
-                    key: '/admin/user',
-                    icon: <UserOutlined />
-                }] : []),
-                ...(viewJob || ACL_ENABLE === 'false' ? [{
-                    label: <Link to='/admin/job'>Job</Link>,
-                    key: '/admin/job',
-                    icon: <ScheduleOutlined />
-                }] : []),
+        ...(viewJob || ACL_ENABLE === 'false' ? [{
+          label: <Link to='/admin/job'>Job</Link>,
+          key: '/admin/job',
+          icon: <ScheduleOutlined />
+        }] : []),
 
-                ...(viewResume || ACL_ENABLE === 'false' ? [{
-                    label: <Link to='/admin/resume'>Resume</Link>,
-                    key: '/admin/resume',
-                    icon: <AliwangwangOutlined />
-                }] : []),
-                ...(viewPermission || ACL_ENABLE === 'false' ? [{
-                    label: <Link to='/admin/permission'>Permission</Link>,
-                    key: '/admin/permission',
-                    icon: <ApiOutlined />
-                }] : []),
-                ...(viewRole || ACL_ENABLE === 'false' ? [{
-                    label: <Link to='/admin/role'>Role</Link>,
-                    key: '/admin/role',
-                    icon: <ExceptionOutlined />
-                }] : []),
+        ...(viewResume || ACL_ENABLE === 'false' ? [{
+          label: <Link to='/admin/resume'>Resume</Link>,
+          key: '/admin/resume',
+          icon: <AliwangwangOutlined />
+        }] : []),
 
+        ...(viewPermission || ACL_ENABLE === 'false' ? [{
+          label: <Link to='/admin/permission'>Permission</Link>,
+          key: '/admin/permission',
+          icon: <ApiOutlined />
+        }] : []),
 
+        ...(viewRole || ACL_ENABLE === 'false' ? [{
+          label: <Link to='/admin/role'>Role</Link>,
+          key: '/admin/role',
+          icon: <ExceptionOutlined />
+        }] : []),
+      ];
 
-            ];
-
-            setMenuItems(full);
-        }
-    }, [permissions])
-    useEffect(() => {
-        setActiveMenu(location.pathname)
-    }, [location])
-
-    const handleLogout = async () => {
-        const res = await callLogout();
-        if (res && +res.statusCode === 200) {
-            dispatch(setLogoutAction({}));
-            message.success('Đăng xuất thành công');
-            navigate('/')
-        }
+      setMenuItems(full);
     }
+  }, [permissions]);
 
-    // if (isMobile) {
-    //     items.push({
-    //         label: <label
-    //             style={{ cursor: 'pointer' }}
-    //             onClick={() => handleLogout()}
-    //         >Đăng xuất</label>,
-    //         key: 'logout',
-    //         icon: <LogoutOutlined />
-    //     })
-    // }
+  useEffect(() => {
+    setActiveMenu(location.pathname);
+  }, [location]);
 
-    const itemsDropdown = [
-        {
-            label: <Link to={'/'}>Trang chủ</Link>,
-            key: 'home',
-        },
-        {
-            label: <label
-                style={{ cursor: 'pointer' }}
-                onClick={() => handleLogout()}
-            >Đăng xuất</label>,
-            key: 'logout',
-        },
-    ];
+  const handleLogout = async () => {
+    const res = await callLogout();
+    if (res && +res.statusCode === 200) {
+      dispatch(setLogoutAction({}));
+      message.success('Đăng xuất thành công');
+      navigate('/');
+    }
+  };
 
-    return (
-        <>
-            <Layout
-                style={{ minHeight: '100vh' }}
-                className="layout-admin"
-            >
-                {!isMobile ?
-                    <Sider
-                        theme='light'
-                        collapsible
-                        collapsed={collapsed}
-                        onCollapse={(value) => setCollapsed(value)}>
-                        <div style={{ height: 32, margin: 16, textAlign: 'center' }}>
-                            <BugOutlined />  ADMIN
-                        </div>
-                        <Menu
-                            selectedKeys={[activeMenu]}
-                            mode="inline"
-                            items={menuItems}
-                            onClick={(e) => setActiveMenu(e.key)}
-                        />
-                    </Sider>
-                    :
-                    <Menu
-                        selectedKeys={[activeMenu]}
-                        items={menuItems}
-                        onClick={(e) => setActiveMenu(e.key)}
-                        mode="horizontal"
-                    />
-                }
+  const itemsDropdown = [
+    {
+      label: <Link to={'/'}>Trang chủ</Link>,
+      key: 'home',
+    },
+    {
+      label: (
+        <label style={{ cursor: 'pointer' }} onClick={() => handleLogout()}>
+          Đăng xuất
+        </label>
+      ),
+      key: 'logout',
+    },
+  ];
 
-                <Layout>
-                    {!isMobile &&
-                        <div className='admin-header' style={{ display: "flex", justifyContent: "space-between", marginRight: 20 }}>
-                            <Button
-                                type="text"
-                                icon={collapsed ? React.createElement(MenuUnfoldOutlined) : React.createElement(MenuFoldOutlined)}
-                                onClick={() => setCollapsed(!collapsed)}
-                                style={{
-                                    fontSize: '16px',
-                                    width: 64,
-                                    height: 64,
-                                }}
-                            />
+  return (
+    <>
+      <Layout style={{ minHeight: '100vh' }} className="layout-admin">
+        {!isMobile ? (
+          <Sider
+            className="admin-sider"
+            theme="dark"
+            collapsible
+            collapsed={collapsed}
+            onCollapse={(value) => setCollapsed(value)}
+            width={240}
+            collapsedWidth={80}
+            trigger={null}
+          >
+            <div className="admin-sider__brand">
+              <img className="admin-sider__logo" src={uetLogo} alt="UET" />
+            </div>
 
-                            <Dropdown menu={{ items: itemsDropdown }} trigger={['click']}>
-                                <Space style={{ cursor: "pointer" }}>
-                                    Welcome {user?.name}
-                                    <Avatar> {user?.name?.substring(0, 2)?.toUpperCase()} </Avatar>
+            <Menu
+              selectedKeys={[activeMenu]}
+              mode="inline"
+              theme="dark"
+              items={menuItems}
+              onClick={(e) => setActiveMenu(e.key)}
+            />
+          </Sider>
+        ) : (
+          <Menu
+            selectedKeys={[activeMenu]}
+            items={menuItems}
+            onClick={(e) => setActiveMenu(e.key)}
+            mode="horizontal"
+          />
+        )}
 
-                                </Space>
-                            </Dropdown>
-                        </div>
-                    }
-                    <Content style={{ padding: '15px' }}>
-                        <Outlet />
-                    </Content>
-                    {/* <Footer style={{ padding: 10, textAlign: 'center' }}>
-                        React Typescript series Nest.JS &copy; Hỏi Dân IT - Made with <HeartTwoTone />
-                    </Footer> */}
-                </Layout>
-            </Layout>
+        <Layout>
+          {!isMobile && (
+            <div className="admin-header">
+              <Button
+                type="text"
+                icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+                onClick={() => setCollapsed(!collapsed)}
+                style={{
+                  fontSize: '16px',
+                  width: 64,
+                  height: 64,
+                }}
+              />
 
-        </>
-    );
+              <Dropdown menu={{ items: itemsDropdown }} trigger={['click']}>
+                <Space style={{ cursor: "pointer" }}>
+                  Welcome {user?.name}
+                  <Avatar>{user?.name?.substring(0, 2)?.toUpperCase()}</Avatar>
+                </Space>
+              </Dropdown>
+            </div>
+          )}
+
+          <Content style={{ padding: 20 }}>
+            <Outlet />
+          </Content>
+        </Layout>
+      </Layout>
+    </>
+  );
 };
 
 export default LayoutAdmin;
